2f91be29b530ac570d2c6a0ef482f05ee00403ce
Arvin Jayson Castro <arvinjaysoncastro@gmail.com>
2026-03-06 02:31:25 +0800

    feat(api): wire infrastructure and application services in Program.cs

--- src/AppointmentManagementSystem.API/Program.cs
+++ src/AppointmentManagementSystem.API/Program.cs
@@ -1,14 +1,24 @@
+using AppointmentManagementSystem.Application.Interfaces;
+using AppointmentManagementSystem.Application.Services;
+using AppointmentManagementSystem.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Controllers
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+// Infrastructure (DbContext + Repositories)
+builder.Services.AddInfrastructure(builder.Configuration);
+
+// Application services
+builder.Services.AddScoped<IAppointmentService, AppointmentService>();
+
+// OpenAPI
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// OpenAPI
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
