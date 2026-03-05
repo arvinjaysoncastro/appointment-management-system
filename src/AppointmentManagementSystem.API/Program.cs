2b42c1913c3d001f3f0fb045e02f301fc42f5d10
Arvin Jayson Castro <arvinjaysoncastro@gmail.com>
2026-03-06 02:46:05 +0800

    feat(infrastructure): enable automatic SQLite database initialization on startup

--- src/AppointmentManagementSystem.API/Program.cs
+++ src/AppointmentManagementSystem.API/Program.cs
@@ -1,6 +1,7 @@
 using AppointmentManagementSystem.Application.Services;
 using AppointmentManagementSystem.API.Configuration;
 using AppointmentManagementSystem.Infrastructure;
+using AppointmentManagementSystem.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +18,17 @@ builder.Services.AddScoped<IAppointmentService, AppointmentService>();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+// Initialize database
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.EnsureCreated();
+}
+
+// Long term, use Database.Migrate();
+// requiring: dotnet ef migrations add Initial
+// for this exam: the implemation above is more practical: EnsureCreated
 
 // Exception handling middleware
 app.UseMiddleware<ExceptionHandlingMiddleware>();
